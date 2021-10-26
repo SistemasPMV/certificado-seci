694f116e71916ba54a468df9c5ac6c90812592fb
13412 <sistemas@votorantim.sp.gov.br>
2021-10-26 12:41:35 -0300

    Adicionado Bootstrap

--- src/main.js
+++ src/main.js
@@ -2,12 +2,19 @@ import Vue from 'vue'
 import App from './App.vue'
 import * as mdbvue from 'mdbvue'
 import router from './router'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
+
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
 
 for (const component in mdbvue) {
   Vue.component(component, mdbvue[component])
 }
 
 Vue.config.productionTip = false
+
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin);
 
 new Vue({
   router,
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,24 @@
 <template>
   <main>
-    <input v-model="nome" type="text" />
     <img
       style="display: none"
       ref="imageCertificado"
       src="../assets/certificado.png"
     />
-    <button @click="generatePDF()">Gerar Certificado</button>
+
+    <div class="container">
+      <div class="row">
+        <div class="form-group col-md-6">
+          <label>Nome:</label>
+          <input class="form-control" v-model="nome" type="text" />
+        </div>
+
+        <div class="form-group col-md-6">
+          <label>&nbsp;</label>
+          <button class="btn btn-primary form-control"  @click="generatePDF()">Gerar Certificado</button>
+        </div>
+      </div>
+    </div>
   </main>
 </template>
 
@@ -35,7 +47,7 @@ export default {
 
       doc.addImage(this.$refs.imageCertificado, "PNG", 0, 0, width, height);
       doc.text(this.nome.toUpperCase(), 110, 106);
-      doc.text('26/10/2021', 98, 133);
+      doc.text("26/10/2021", 98, 133);
       //doc.save("certificado.pdf");
       doc.output("dataurlnewwindow");
     },
